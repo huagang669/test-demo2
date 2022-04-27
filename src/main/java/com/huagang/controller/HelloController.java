668ee41019353e9af26b8151f8b2cdbec44d203b
huagang <2462271845@qq.com>
2022-04-27 17:05:54 +0800

    hot-fix merge commit

--- src/main/java/com/huagang/controller/HelloController.java
+++ src/main/java/com/huagang/controller/HelloController.java
@@ -4,7 +4,7 @@ package com.huagang.controller;
 public class HelloController {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world hot-fix");
         System.out.println("third commit");
         System.out.println("hot-fix-test01");
     }
